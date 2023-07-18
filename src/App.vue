e1233c0acb52ba3495bbd2ebd5376f1cc3f238a5
Pegah Kazemi <p.kazemi@Rayan.Parsi>
2023-07-18 09:40:53 +0330

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Math Quiz App</h1>
+        <button>Addition<br/>+</button>
+        <button>Subtraction<br/>-</button>
+        <button>Division<br/>/</button>
+        <button>Multiplication<br/>*</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+button {
+    padding: 10px;
+    width: 200px;
+    margin-right: 10px;
+    height: 80px;
+    background-color: #10aeb2;
+    color: white;
+    font-size: 24px;
+    cursor: pointer;
+    vertical-align: top;
+    border: none;
 }
 </style>

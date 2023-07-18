6b39120c84c78c1f31751f2c58f7cf67f9bf4528
Pegah Kazemi <p.kazemi@Rayan.Parsi>
2023-07-18 10:59:56 +0330

    update operatorQuiz

--- src/components/OperatorQuiz.vue
+++ src/components/OperatorQuiz.vue
@@ -2,19 +2,44 @@
 
 export default {
     name: "OperatorQuiz",
-    props: ['operator']
+    props: ['operator'],
+    data() {
+        return {
+            operandLeft: null,
+            operandRight: null,
+            isQuizStarted: false,
+            answers:[]
+        }
+    },
+    methods: {
+        starQuiz() {
+            this.isQuizStarted = true
+            this.operandLeft = parseInt(Math.random() * 13)
+            this.operandRight = parseInt(Math.random() * 13)
+        }
+    },
 }
 </script>
 
 <template>
     <div>
-        <h1>
-            {{ operator }}
-        </h1>
-        <button @click="$emit('onBack')">Back</button>
+        <div v-if="isQuizStarted">
+            <h4>{{ operandLeft }} {{ operator }} {{ operandRight }}</h4>
+        </div>
+        <div class="quiz">
+            <div v-if="!isQuizStarted">
+                <button @click="starQuiz">Start</button>
+            </div>
+            <button @click="$emit('onBack')">Back</button>
+        </div>
     </div>
 </template>
 
 <style scoped>
-
+.quiz {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
 </style>
